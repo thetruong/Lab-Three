183e8343be110114eb77232e8177fc9c69d84492
thetruong <thetruong@users.noreply.github.com>
2015-01-25 17:34:54 -0800

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,7 @@ var HelloMessage = React.createClass({
 	render: function () {
 		return <div>Hello, {this.props.subject}!</div>
 	}
-})
+});
 
 React.render(<HelloMessage subject='Nerd'/>, document.body);
 
