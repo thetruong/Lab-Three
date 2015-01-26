f0e241a61368479657590acb48f78cd477a0ab79
thetruong <thetruong@users.noreply.github.com>
2015-01-25 17:32:38 -0800

    Update bundle.js

--- src/bundle.js
+++ src/bundle.js
@@ -9,28 +9,7 @@ var HelloMessage = React.createClass({displayName: "HelloMessage",
 
 React.render(React.createElement(HelloMessage, {subject: "Nerd"}), document.body)
 
-// var MyWidget = React.createClass({
-//   handleClick: function() {
-//     alert('Hello!');
-//   },
-//   render: function() {
-//     return (
-//       <a href="#" onClick={this.handleClick}>Do something!</a>
-//     );
-//   }
-// });
 
-// var MyView = Backbone.View.extend({
-//   el: 'body',
-//   template: '<div class="widget-container"></div>',
-//   render: function() {
-//     this.$el.html(this.template);
-//     React.renderComponent(new MyWidget(), this.$('.widget-container').get(0));
-//     return this;
-//   }
-// });
-
-// new MyView().render();
 },{"react":148}],2:[function(require,module,exports){
 // shim for using process in browser
 
